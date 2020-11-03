0fbfb060c6f15d6f017184f8ef9daea40b58ea38
paolo-32 <65409747+paolo-32@users.noreply.github.com>
2020-11-03 14:18:48 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,128 @@
+var obj = {
+    "abbassamento_della_voce":["Sindrome dell'ovaio policlistico", "tumore alla laringe"],
+    "acatisia":["demenza", "disturbi isterici", "Parkinson"],
+    "acinesia":["Parkinson", "schizofrenia catatonica"],
+    "acufene":["Meningite", "Sifilide", "Ictus"],
+    "aerofagia":["Pancreatite", "Tumore all'ovaio"],
+    "afagia":["Esofagite", "Tumore all'esofago"],
+    "afasia":["Botulismo", "Ictus", "Alzheimer", "Sclerosi multipla"],
+    "aftosi_orale":["Morbo di crohn"],
+    "alopecia":["Scabbia", "Sifilide", "Sindrome dell'ovaio policistico"],
+    "aprassia":["Alzheimer", "Parkinson"],
+    "astenia":["AIDS", "Botulismo", "Cancro al colon", "Ebola", "Epatite", "Insufficienza renale", "Meningite", "Sindrome premestruale"],
+    "batteriuria":["Clamidia", "Cistite"],
+    "blefaroptosi":["Botulismo", "Diabete", "Ictus"],
+    "bocca_amara":["Tumore allo stomaco"],
+    "bolle":["Herpes simplex", "Scabbia"],
+    "braccia_gonfie":["Aneurisma", "Cellululite"],
+    "brividi":["Calcoli renali", "Ebola", "Prostatite", "Tubercolosi"],
+    "cachessia":["AIDS", "Ebola", "Tifo", "Tubercolosi"],
+    "calo_libido":["Cirrosi", "Disfunzione erettile"],
+    "capelli_fragili":["AIDS"],
+    "convulsioni":["Ebola", "Meningite", "Toxoplasmosi"],
+    "crampi_addominali":["Appendicite", "Cancro al colon"],
+    "dissenteria":["AIDS", "Ebola"],
+    "diarrea":["AIDS", "Ebola"],
+    "disturbi_del_linguaggio":["Ictus"],
+    "disartria":["Ictus"],
+    "dolori_articolari":["AIDS"],
+    "dolori_muscolari":["AIDS"],
+    "perdita_di_peso":["AIDS"],
+    "disfagia":["AIDS", "Ebola", "Mononucleosi", "Tumore allo stomaco"],
+    "disfonia":["Botulismo"],
+    "dispnea":["AIDS", "Cirrosi epatica"],
+    "disuria":["Cancro alla prostata", "Cancro alla vescica", "Clamidia", "Gonorrea", "Herpes genitale", "Prostatite", "Vaginite"],
+    "eiaculazione_dolorosa":["Cancro alla prostata", "Prostatite", "Infezioni"],
+    "ematemesi":["Ebola", "Tumore all'esofago"],
+    "emianopsia":["Ictus", "Meningite"],
+    "emiplegia":["Ictus", "Diabete"],
+    "emoglobinuria":["Anemia falciforme", "Anemia mediterranea", "Tubercolosi"],
+    "emorragia_gastrointestinale":["Cirrosi epatica", "Ebola", "E coli", "Tumore allo stomaco", "Tumore colon-retto", "Tumore al fegato", "Tumore dell'ano", "Tumore all'esofago"],
+    "emottisi":["BPCO", "Ebola", "Embolia polmonare", "Tubercolosi", "Tumore al polmone"],
+    "epatomegalia":["Cirrosi epatica", "Ebola", "Epatite", "Sifilide", "Tumore al fegato"],
+    "eritema":["Cirrrosi epatica", "Lebbra", "Scabbia"],
+    "eritocitosi":["Tumore al fegato", "Tumore al rene"],
+    "esantema":["Ebola", "Morbillo", "Sifilide", "Vaiolo"],
+    "escara":["intossicazione da antraace"],
+    "febbre":["AIDS"],
+    "galattorrea":["Cancro al seno", "insufficenza renale"],
+    "ginecomastia":["Tumore ai testicoli", "lebbra"],
+    "idrofobia":["Rabbia"],
+    "ingrossamento_delle_parotidi":["Cirrosi epatica", "Mononucleosi", "Tonsillite"],
+    "ipertesia":["Meningite", "Rabbia"],
+    "ipocausia":["Meningite", "Sifilide", "Sindrome di Down"],
+    "ipoestesia":["Lebbra"],
+    "ipotensione":["Anemia", "Ebola", "Insufficienza cardiaca", "Malaria"],
+    "ipovolemia":["Ebola", "Insufficienza renale", "Malaria"],
+    "ittero":["AIDS", "Ebola", "Epatite", "Tumore al fegato", "Tumore al pancreas"],
+    "labbra_gonfie":["Allergia alimentare", "Herpes simplex"],
+    "leucopenia":["AIDS", "Cirrosi epatica", "Epatite", "Morbillo", "Tubercolosi"],
+    "linfonodi_ingrossati":["AIDS", "Ebola", "Sifilide", "Tubercolosi", "Tumore dell'ano"],
+    "lividi":["Ebola", "Leucemia", "Cirrosi epatica"],
+    "macroglossia":["Sindrome di Down"],
+    "macule":["Ebola", "Morbillo", "Lebbra"],
+    "mal_di_gola":["AIDS", "Clamidia", "Ebola", "Gonorrea", "Sifilide"],
+    "mal_di_stomaco":["Sifilide"],
+    "mal_di_testa":["AIDS", "Ebola",  "Ictus", "Meningite", "Sifilide"],
+    "meteorismo":["Cancro al colon", "Appendicite"],
+    "midriasi":["Ictus"],
+    "mobilità_dei_denti":["Menopausa"],
+    "naso_chiuso":["Lebbra", "Rinite", "Sinusite"],
+    "nausea":["Botulismo", "Cirrosi epatica", "Ebola", "Epatite", "Tumori"],
+    "nodulo":["Cancro alla prostata", "Tumori"],
+    "occhi_arrossati":["Ebola"],
+    "occhi_disallineati":["Ictus", "Meningite", "Miopia", "Sindrome di Down", "Strabismo"],
+    "oliguria":["Cirrosi epatica", "Ebola"],
+    "onissi":["Candida"],
+    "pallore":["Cancro al colon", "Cirrosi epatica", "Meningite"],
+    "parestesia":["Ebola", "Herpes zoster", "Ictus"],
+    "perdita_di_memoria":["AIDS", "Ictus", "Demenza", "Sifilide", "Parkinson"],
+    "perdita_di_coordinazion_dei_movimenti":["Attacco ischemico transotorio", "Encefalite", "Sifilide"],
+    "perdita_di_equilibrio":["Ictus", "Artrite"],
+    "perdita_di_ricordi":["Demenza", "Alzheimer"],
+    "perdite_uretrali":["Candida", "Clamidia", "Epididimite", "Gonorrea", "Prostatite"],
+    "perdite_vaginali":["Candida", "Clamidia", "Gonorrea", "Vaginite"],
+    "perionissi":["Herpes simplex"],
+    "poliuria":["Cancro alla prostata", "Malaria", "Prostatite"],
+    "pollachiuria":["Calcoli renali", "Cancro alla prostata"],
+    "postite":["Candida", "Scabbia", "Sifilide"],
+    "riduzione_della_vista":["Herpes simplex", "Ictus"],
+    "salivazione_intensa":["Ictus", "Sifilide", "Tubercolosi"],
+    "sangue_dal_naso":["AIDS", "Cirrosi epatica", "Lebbra", "Tifo"],
+    "sangue_nel_catarro":["Bronchite", "Ebola", "Tubercolosi"],
+    "sangue_nelle_urine":["Anemia falciforme", "Cancro prostata", "Clamidia", "Ebola", "Gonorrea", "Tumore ai testicoli"],
+    "sangue_nello_sperma":["Tubercolosi", "Tumore ai testicoli"],
+    "sanguinamento_dall'orecchio":["Ebola"],
+    "sanguinamento_gengivale":["Ebola", "Gengivite"],
+    "sanguinamento_facile":["Ebola", "Hiv"],
+    "sanguinamento_vaginale":["Candida", "Clamidia", "Ebola", "Gonorrea", "Vaginite"],
+    "secchezza_vaginale":["Candida", "Vaginite"],
+    "singhiozzo":["Ebola", "Encefalite", "Ictus", "Meningite"],
+    "tosse":["AIDS",  "Bronchite", "Ebola", "Fibrosi cistica", "Meningite", "Tubercolosi", "Tumore ai polmoni", "Tumore all'esofago"],
+    "trisma":["Ebola", "Meningite", "Tetano", "Tonsillite"],
+    "ulcere_cutanee":["Ebola", "Sifilide"],
+    "vertigini":["Ebola", "Emicrania", "Ictus", "Sifilide"]
+}
+
+function getInputValue(){
+    var inputVal = document.getElementById("search").value.toLowerCase();
+    var formattedString ;
+    if(/\s/.test(inputVal)){
+        formattedString = inputVal.split(' ').join('_');
+    }else{
+        formattedString = inputVal
+    }
+
+    //alert(formattedString)
+
+    for(var i in obj){
+        if(i === formattedString){
+           var arr = obj[i];
+           var randomDisease = Math.floor(Math.random() * arr.length);
+           //alert(arr[randomDisease])
+           var Output = (arr[randomDisease]) + "🥳";
+           alert(Output);
+           break;
+        }
+    }
+}
